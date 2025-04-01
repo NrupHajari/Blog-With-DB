6841d6823f973c14f7b508b2771eba09b99b3565
Nrup Hajari <nruphajari@gmail.com>
2025-04-01 09:44:52 +0530

    Daily log updates

---
+++ app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Welcome to our blog! Here you'll find a variety of posts on different topics. Dive in and explore our latest thoughts and ideas. We believe in sharing knowledge and inspiring our readers with meaningful content.";
+const aboutContent = "We are a group of passionate writers and creators who love to share ideas, stories, and knowledge. Our blog is a platform where we express our thoughts on various topics, aiming to connect with like-minded individuals and spark conversations.";
+const contactContent = "Got a question or just want to get in touch? We'd love to hear from you! Whether it's feedback, suggestions, or inquiries, feel free to reach out. Let's connect and keep the conversation going!";
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useUnifiedTopology: true }, {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
